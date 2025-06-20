3738c54294cedc0c9d33c4549cde56e84cffec01
jacklion710 <jack.lion710@gmail.com>
2025-06-19 22:04:28 -0400

    Initialization

---
+++ swift-camera-test/swift_camera_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_camera_testApp.swift
+//  swift-camera-test
+//
+//  Created by Jacob Leone on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_camera_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-camera-testTests/swift_camera_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_camera_testTests.swift
+//  swift-camera-testTests
+//
+//  Created by Jacob Leone on 6/19/25.
+//
+
+import Testing
+@testable import swift_camera_test
+
+struct swift_camera_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
